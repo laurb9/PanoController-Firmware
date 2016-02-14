a69ee3c1ce71137014e5b12a3d0fa53f1742b4ab
Laurentiu Badea <laurb9@gmail.com>
2016-02-14 13:30:20 -0800

    Added joystick functionality (polling-based)

--- joystick.cpp
+++ joystick.cpp
@@ -7,11 +7,59 @@
  * A copy of this license has been included with this distribution in the file LICENSE.
  */
 
+#include "Arduino.h"
 #include "joystick.h"
 
 Joystick::Joystick(int sw_pin, int x_pin, int y_pin)
 :sw_pin(sw_pin),
  x_pin(x_pin),
  y_pin(y_pin)
-{}
+{
+    pinMode(sw_pin, INPUT_PULLUP);
+    //pinMode(x_pin, INPUT);
+    //pinMode(y_pin, INPUT);
+    sw_state = HIGH;
+    x_state = mid_level;
+    y_state = mid_level;
+    read();
+}
 
+unsigned Joystick::read(void){
+    unsigned event = EVENT_NONE;
+    int current_state;
+
+    if (millis() - last_read < 100){
+        return EVENT_NONE;
+    }
+
+
+    // read click switch
+    current_state = digitalRead(sw_pin);
+    if (current_state != sw_state){
+        sw_state = current_state;
+        if (sw_state == LOW){
+            event |= EVENT_CLICK;
+        }
+    }
+
+    // read X position
+    current_state = (analogRead(x_pin)+(1<<(sensitivity-1))) >> sensitivity;
+    if (current_state > x_state && current_state > mid_level){
+        event |= EVENT_RIGHT;
+    } else if (current_state < x_state && current_state < mid_level){
+        event |= EVENT_LEFT;
+    }
+    x_state = current_state;
+
+    // read Y position
+    current_state = (analogRead(y_pin)+(1<<(sensitivity-1))) >> sensitivity;
+    if (current_state > y_state && current_state > mid_level){
+        event |= EVENT_DOWN;
+    } else if (current_state < y_state && current_state < mid_level){
+        event |= EVENT_UP;
+    }
+    y_state = current_state;
+
+    last_read = millis();
+    return event;
+}
--- joystick.h
+++ joystick.h
@@ -9,11 +9,30 @@
 #ifndef JOYSTICK_H_
 #define JOYSTICK_H_
 
+#define EVENT_NONE  0
+#define EVENT_UP    0b00001
+#define EVENT_DOWN  0b00010
+#define EVENT_LEFT  0b00100
+#define EVENT_RIGHT 0b01000
+#define EVENT_CLICK 0b10000
+
+#define isEventUp(event) (event & EVENT_UP)
+#define isEventDown(event) (event & EVENT_DOWN)
+#define isEventLeft(event) (event & EVENT_LEFT)
+#define isEventRight(event) (event & EVENT_RIGHT)
+#define isEventClick(event) (event & EVENT_CLICK)
+
 class Joystick {
+private:
+    static const int sensitivity = 8; // lower is higher :)
+    static const int mid_level = 512 >> 8;
 protected:
     int sw_pin, x_pin, y_pin;
+    int sw_state = 0, x_state, y_state;
+    int last_read;
 public:
     Joystick(int sw_pin, int x_pin, int y_pin);
+    unsigned read(void);
 };
 
 #endif /* JOYSTICK_H_ */
