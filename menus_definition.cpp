6e9fc502c043dc5787461ab23017071ba829de5f
Laurentiu Badea <laurb9@gmail.com>
2016-07-31 16:10:27 -0700

    Allocate successive EEPROM addresses automatically.
    Hardcoding the index is prone to error.

--- menus_definition.cpp
+++ menus_definition.cpp
@@ -28,8 +28,16 @@ extern volatile int aspect;
 extern volatile int motors_enable;
 extern volatile int display_invert;
 
-#define EEPROM_ADDR(index) (index * sizeof(int))
 #define NO_EEPROM 0
+#define USE_EEPROM assignEEPROM()
+
+/*
+ * allocate successive EEPROM addresses (each call returns the next available int-sized cell)
+ */
+int assignEEPROM(){
+    static int location = 0;
+    return ++location * sizeof(int);
+}
 
 Menu menu("Main Menu", 4, new BaseMenu* const[4] {
 
@@ -38,27 +46,27 @@ Menu menu("Main Menu", 4, new BaseMenu* const[4] {
         new OptionSelector("Repeat Last", NULL, 0, NO_EEPROM, onRepeat),
         new OptionSelector("360 Pano", NULL, 0, NO_EEPROM, on360),
         new OptionSelector("Last Pano Info", NULL, 0, NO_EEPROM, onPanoInfo),
-        new RangeSelector("Manual Horiz FOV", &horiz, 120, EEPROM_ADDR(1), NULL, 10, 360, 10),
-        new RangeSelector("Manual Vert FOV", &vert, 90, EEPROM_ADDR(2), NULL, 10, 180, 10)
+        new RangeSelector("Manual Horiz FOV", &horiz, 120, USE_EEPROM, NULL, 10, 360, 10),
+        new RangeSelector("Manual Vert FOV", &vert, 90, USE_EEPROM, NULL, 10, 180, 10)
     }),
 
    new Menu("Camera", 8, new BaseMenu* const[8] {
-        new ListSelector("Focal Len", &focal, 35, EEPROM_ADDR(3), NULL, 16,
+        new ListSelector("Focal Len", &focal, 35, USE_EEPROM, NULL, 16,
             (const int[]){12, 14, 16, 20, 24, 28, 35, 50, 75, 105, 200, 300, 400, 450, 500, 600}),
-        new NamedListSelector("Shutter", &shutter, 10, EEPROM_ADDR(4), NULL, 11,
+        new NamedListSelector("Shutter", &shutter, 10, USE_EEPROM, NULL, 11,
             (const char * const[]){"1/100", "1/50", "1/25", "1/10", "1/4", "0.5s", "1s", "2s", "4s", "8s", "MANUAL"},
             (const int[]){10, 20, 40, 100, 250, 500, 1000, 2000, 4000, 8000, 0}),
-        new NamedListSelector("Delay", &pre_shutter, 100, EEPROM_ADDR(5), NULL, 6,
+        new NamedListSelector("Delay", &pre_shutter, 100, USE_EEPROM, NULL, 6,
             (const char * const[]){"0.1s", "0.5s", "1s", "2s", "4s", "8s"},
             (const int[]){100, 500, 1000, 2000, 4000, 8000}),
-        new NamedListSelector("Processing Wait", &post_wait, 100, EEPROM_ADDR(6), NULL, 7,
+        new NamedListSelector("Processing Wait", &post_wait, 100, USE_EEPROM, NULL, 7,
             (const char * const[]){"0.1s", "0.25s", "0.5s", "1s", "2s", "4s", "8s"},
             (const int[]){100, 250, 500, 1000, 2000, 4000, 8000}),
-        new NamedListSelector("Shutter Mode", &long_pulse, 0, EEPROM_ADDR(7), NULL, 2,
+        new NamedListSelector("Shutter Mode", &long_pulse, 0, USE_EEPROM, NULL, 2,
             (const char * const[]){"Normal", "Cont Bkt"},
             (const int[]){0, 1}),
-        new RangeSelector("Shots #", &shots, 1, EEPROM_ADDR(8), NULL, 1, 5, 1),
-        new NamedListSelector("Aspect", &aspect, 23, EEPROM_ADDR(9), NULL, 2,
+        new RangeSelector("Shots #", &shots, 1, USE_EEPROM, NULL, 1, 5, 1),
+        new NamedListSelector("Aspect", &aspect, 23, USE_EEPROM, NULL, 2,
             (const char * const[]){"P 2:3", "L 3:2"},
             (const int[]){23, 32}),
         new OptionSelector("Test Shutter", NULL, 0, NO_EEPROM, onTestShutter)
