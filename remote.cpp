laurb9/PanoController-Firmware

commit a9a181cfd6d45865cb5dd53efc2fe38c3a385516

Fix key repeat bug in IR remote.
Ignore keys when remote sends repeated codes for a single press.

remote.cpp
@@ -61,9 +61,13 @@ unsigned Remote::read(void){
             Serial.print("Unknown remote code ");
             Serial.println(code, HEX);
         };
-        if (event && last_event != event){
-            last_event = event;
-            next_repeat_time = millis() + REPEAT_DELAY;
+        // detect and ignore a too-fast repeat of same key
+        if (event){
+            if (last_event == event && millis() < next_repeat_time){
+                event = EVENT_NONE;
+            } else {
+                next_repeat_time = millis() + REPEAT_DELAY;
+            }
         }
     }
     return event;

commit d4eefb2c51514ab74b53036d6c12c64833b2eb19

Fix repeat code in IR remote broken by a9a181c
Commit a9a181c forgot to remember which key was pressed, so repeat code
did not work at all.

remote.cpp
@@ -67,6 +67,7 @@ unsigned Remote::read(void){
                 event = EVENT_NONE;
             } else {
                 next_repeat_time = millis() + REPEAT_DELAY;
+                last_event = event;
             }
         }
     }