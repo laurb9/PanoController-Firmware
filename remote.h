c51f734297eb8832f4ab604b3da538765dabf474
Laurentiu Badea <laurb9@gmail.com>
2016-08-09 23:56:12 -0700

    Stub IRremote #59
    The library currently does not compile for this target, so we create a
    stub Remote class.
    
    See https://github.com/z3t0/Arduino-IRremote/issues/274

--- remote.h
+++ remote.h
@@ -9,9 +9,14 @@
 #ifndef REMOTE_H_
 #define REMOTE_H_
 
+#include "hid.h"
+
+#if !defined(ARDUINO_SAMD_ZERO)
+
 #include <Arduino.h>
 #include <IRremote.h>
-#include "hid.h"
+#include <IRremoteInt.h>
+#include <IRremoteTools.h>
 
 // the remote I have sends repeat code too quick
 // allow repeat code only after this many milliseconds
@@ -24,11 +29,20 @@ private:
     unsigned next_repeat_time;
     IRrecv* remote;
     decode_results remote_buffer;
-protected:
 public:
     Remote(int pin);
     unsigned read(void) override;
     int clear(int timeout);
 };
 
+#else /* defined(ARDUINO_SAMD_ZERO) */
+
+class Remote : public HID {
+public:
+    Remote(int pin){ };
+    unsigned read(void) override { return EVENT_NONE; };
+    int clear(int timeout) override { return timeout; };
+};
+
+#endif /* defined(ARDUINO_SAMD_ZERO) */
 #endif /* REMOTE_H_ */
